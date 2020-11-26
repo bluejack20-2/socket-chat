95f8af6582e0730163d700aa1d6c30942c263fde
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-11-26 16:04:48 +0700

    chat row redesign

--- src/components/chat-row.jsx
+++ src/components/chat-row.jsx
@@ -7,7 +7,7 @@ const ChatRow = ({ children, username }) => {
     <div className={'rounded p-4 m-4 ' + (isSelf ? 'bg-green-500' : 'bg-white')}>
       <p className="text-xl font-bold">{children.user}</p>
       <p className="text-lg">{children.text}</p>
-      <p className={'text-xs ' + (isSelf ? 'text-white' : 'text-gray-600')}>
+      <p className={'text-xs ' + (isSelf ? 'text-gray-700' : 'text-gray-600')}>
         {new Date(children.timestamp).toLocaleString()}
       </p>
     </div>
